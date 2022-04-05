b44e5ba143f0b90c8655151e2f98072a49d77711
Jiaming Wang <jiamingwang.net@gmail.com>
2022-04-05 12:42:18 +1000

    re-ordered files

--- js/globalTags.js
+++ js/globalTags.js
@@ -1,7 +1,7 @@
 document.querySelector("#nav").innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark nav-bg nav">
     
-<a class="navbar-brand" href="/"><img src="images/icon.png" alt="Icon" width="30%" style="margin-right: 10px;">Home</a>
+<a class="navbar-brand" href="/"><img src="/images/icon.png" alt="Icon" width="30%" style="margin-right: 10px;">Home</a>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
   aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
@@ -9,13 +9,13 @@ document.querySelector("#nav").innerHTML = `
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
     <li class="nav-item">
-      <a class="nav-link" href="games.html">Games</a>
+      <a class="nav-link" href="/games/">Games</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="projects.html">Projects</a>
+      <a class="nav-link" href="/projects/">Projects</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="projects/Chat/chat.html">Chat</a>
+      <a class="nav-link" href="/projects/Chat/chat.html">Chat</a>
     </li>
   </ul>
 
