68dafa8865091c2076c49cb7ca6084098057ddb3
Jiaming Wang <jiamingwang.net@gmail.com>
2022-04-05 12:20:32 +1000

    updated index directory

--- js/globalTags.js
+++ js/globalTags.js
@@ -1,7 +1,7 @@
 document.querySelector("#nav").innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark nav-bg nav">
     
-<a class="navbar-brand" href="index.html"><img src="images/icon.png" alt="Icon" width="30%" style="margin-right: 10px;">Home</a>
+<a class="navbar-brand" href="/"><img src="images/icon.png" alt="Icon" width="30%" style="margin-right: 10px;">Home</a>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
   aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
